9a9be45f9dba754d8c3912550fc09e4bf32b9f29
MiiaHuvila <mangamirkku@gmail.com>
2016-09-08 09:21:20 +0300

    Sosiaalisenmedian logoja lisatty

--- dad/kuva-miia.php
+++ dad/kuva-miia.php
@@ -107,21 +107,25 @@
     <div class="large-4 column">
 		<h4><i class="fi-torsos-all"></i> SoMETTista</h4>
 		<hr>
-		<p>Jotain paskaa tekstiä tässäkin ehkä mahdollisesti saattaakin näkyä tai sitten
-		ehkä mahdollisesti ei.</p>
+		<p>SoMETTi on Erikoistietotoimiston Musketin sosiaalisenmedian nettiversio,
+		joka mahdollistaa tavallisten ihmisten kuvien lähettämisen yhteisön ja
+		Erikoistietotoimiston työntekijöiden tutkittavaksi.</p>
 	</div>
     <div class="large-4 column">
 		<h4><i class="fi-book"></i> Käyttäjäehdot</h4>
 		<hr>
-		<p>Jotain paskaa tekstiä tässäkin ehkä mahdollisesti saattaakin näkyä tai sitten
-        ehkä mahdollisesti ei.</p>
+		<p>Huomioithan, että sinulla tulee olla oikeudet lähettämääsi kuvaan ja
+		kuva ei saa sisältää mitään sopimatonta materiaalia tai se tullaan 
+		poistamaan palvelusta lopullisesti.</p>
 	</div>
     <div class="large-4 column">
 		<h4><i class="fi-magnifying-glass"></i> Löydä meidät</h4>
 		<hr>
-		  <a href="#"><i class="fi-social-facebook"></i></a>&nbsp;
-		  <a href="#"><i class="fi-social-twitter"></i></a>&nbsp; 
-		  <a href="#"><i class="fi-social-pinterest"></i></a>
+		  <a href="https://fi-fi.facebook.com/"><i class="fi-social-facebook"></i></a>&nbsp;
+		  <a href="https://twitter.com/?lang=fi"><i class="fi-social-twitter"></i></a>&nbsp; 
+		  <a href="https://fi.pinterest.com/"><i class="fi-social-pinterest"></i></a>&nbsp;
+		  <a href="https://www.reddit.com/"><i class="fi-social-reddit"></i></a>&nbsp;
+		  <a href="https://plus.google.com/"><i class="fi-social-google-plus"></i></a>
 	</div>
 	</div>
 </section>
