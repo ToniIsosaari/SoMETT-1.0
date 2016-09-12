83fa71e7f047987e43b06c85aa40dd97ca44e2a1
MiiaHuvila <mangamirkku@gmail.com>
2016-09-12 16:36:27 +0300

    Pinta pinta remonttia

--- SoMETT/index.php
+++ SoMETT/index.php
@@ -46,29 +46,40 @@
   <!-- Main osio (lisää sivulle olennainen sisältö tänne) -->
 <section class="main etusivu">
   <div class="wrap">
-      <h1>SoMETT</h1>
       <p>"Vähän niin kuin Musketti, mutta vain sata kertaa parempi"</p>
+      <h1>SoMETT</h1>
   </div>
 </section>
   <!-- Secondary osio (lisää eri niin tärkeä sisältö tänne) -->
 <section class="secondary">
   <div class="wrap">
-    <h2>Mikä on SoMETT?</h2>
-    <hr>
-    <p>SoMETTi on Erikoistietotoimiston Musketin sosiaalisenmedian nettiversio, joka mahdollistaa 
-    tavallisten ihmisten kuvien lähettämisen yhteisön ja Erikoistietotoimiston työntekijöiden tutkittavaksi.
-    Tavoitteenamme on edistää museoaineiston tutkimista käyttämällä hyödyksi innostuneita vapaaehtoisia yhteisönjäseniä,
-    liity joukkoomme jo tänään!</p>
-    <h2>Miten SoMETT toimii?</h2>
-    <hr>
-    <p>SoMETTin toiminnan pääpilari on sosiaalisuus ja tärkeimpänä työkalunamme ovat sosiaalisenmedian eri palvelut.
-    Meille on hyvin tärkeää kasvattaa laaja käyttäjäkunta, joka mahdollistaa sen, että palveluumme lisätään aktiivisesti
-    uusia kuvia ja jo olemassa oleviin kuviin saataisiin kerättyä tarpeeksi aiheeseen liittyvää tietoa.</p>
-    <p><h4>Toimintamallimme pähkinänkuoressa:</h4>
-    1. Käyttäjä lisää kuvan palveluumme ja jakaa kuvan sosiaalisen medianpalveluihin antamamme linkin kautta.<br>
-    2. Muut käyttäjät kommentoivat kuvaa ja kertovat tietonsa kyseiseen aiheeseen liittyen.<br>
-    3. Erikoistietotoimiston työntekijä varmistaa tiedot.<br>
-    4. Museoviraston työntekijä päättää liitetäänkö kuvamateriaali museotietokantaan.</p>
+      <h2>Mikä on SoMETT?</h2>
+      <hr>
+      <p>SoMETTi on Erikoistietotoimiston Musketin sosiaalisenmedian nettiversio, joka mahdollistaa 
+      tavallisten ihmisten kuvien lähettämisen yhteisön ja Erikoistietotoimiston työntekijöiden tutkittavaksi.
+      Tavoitteenamme on edistää museoaineiston tutkimista käyttämällä hyödyksi innostuneita vapaaehtoisia yhteisönjäseniä,
+      liity joukkoomme jo tänään!</p>
+  </div>
+</section>
+<section class="secondary grey">
+  <div class="wrap">
+      <h2>Miten SoMETT toimii?</h2>
+      <hr>
+      <div class="row">
+        <div class="small-12 medium-6 large-6 columns">
+          <p>SoMETTin toiminnan pääpilari on sosiaalisuus ja tärkeimpänä työkalunamme ovat sosiaalisenmedian eri palvelut.
+          Meille on hyvin tärkeää kasvattaa laaja käyttäjäkunta, joka mahdollistaa sen, että palveluumme lisätään aktiivisesti
+          uusia kuvia ja jo olemassa oleviin kuviin saataisiin kerättyä tarpeeksi aiheeseen liittyvää tietoa.</p>
+        </div>
+        <div class="small-12 medium-6 large-6 columns">
+          <h4>Toimintamallimme pähkinänkuoressa:</h4>
+          <p>
+          1. Käyttäjä lisää kuvan palveluumme ja jakaa kuvan sosiaalisen medianpalveluihin antamamme linkin kautta.<br>
+          2. Muut käyttäjät kommentoivat kuvaa ja kertovat tietonsa kyseiseen aiheeseen liittyen.<br>
+          3. Erikoistietotoimiston työntekijä varmistaa tiedot.<br>
+          4. Museoviraston työntekijä päättää liitetäänkö kuvamateriaali museotietokantaan.</p>
+        </div> 
+      </div>
   </div>
 </section>
   <!-- Footer osio (ÄLÄ KOSKE!) -->
