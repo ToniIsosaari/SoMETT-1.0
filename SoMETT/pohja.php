34ae47326070169dbff6c45fde941c7a6d4a1ba5
MiiaHuvila <mangamirkku@gmail.com>
2016-09-12 13:23:36 +0300

    pohja

---
+++ SoMETT/pohja.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="fi">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="x-ua-compatible" content="ie=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<title>SoMETT</title>
+<link rel="stylesheet" href="http://cosmo.kpedu.fi/~tomijylha/SoMETT-1.0/dad/dropzone-4.3.0/dist/dropzone.css">
+<link rel="stylesheet" href="http://cosmo.kpedu.fi/~miiahuvila/somett/SoMETT-1.0/dad/node_modules/foundation-sites/dist/foundation.min.css">
+<link rel="stylesheet" href="foundation-icons/foundation-icons.css">
+<link rel="stylesheet" href="tyylit.css">
+</head>
+<body>
+<div class="off-canvas-wrapper">
+ <div class="off-canvas-wrapper-inner" data-off-canvas-wrapper>
+ 	<div class="off-canvas position-left" id="offCanvas" data-off-canvas>
+  <!-- Mobiili Navipalkki (ÄLÄ KOSKE!) -->
+<ul class="vertical menu mobile-menu">
+ <li><a href="http://cosmo.kpedu.fi/~miiahuvila/somett/SoMETT-1.0/SoMETT/index.php">Etusivu</a></li>
+ <li><a href="#">Kuvapankki</a></li>
+ <li><a href="#">Lähetyspalvelu</a></li>
+</ul>
+
+</div>
+
+<div class="off-canvas-content" data-off-canvas-content>
+   <div class="title-bar nav-mobile">
+  <div class="title-bar-left">
+    <button class="menu-icon" type="button" data-open="offCanvas"></button>
+    <span class="title-bar-title">SoMETT</span>
+  </div>
+</div>
+  <!-- Poytakone Navipalkki (ÄLÄ KOSKE!) -->
+<div class="top-bar nav-desktop">
+<div class="wrap">
+  <div class="top-bar-left">
+    <ul class="dropdown menu dropdown-desktop" data-dropdown-menu>
+      <li class="menu-text">SoMETT</li>
+      <li><a href="http://cosmo.kpedu.fi/~miiahuvila/somett/SoMETT-1.0/SoMETT/index.php">Etusivu</a></li>
+      <li><a href="#">Kuvapankki</a></li>
+      <li><a href="#">Lähetyspalvelu</a></li>
+    </ul>
+  </div>
+</div>
+</div>
+  <!-- Main osio (lisää sivulle olennainen sisältö tänne) -->
+<section class="main">
+  <div class="wrap">
+    <h1>Pääotsikko</h1>
+    <p>Tähän tulee sivun pääsisältö muistathan, ettei sinulla ole oikeuksia muuttaa section nimeä, vaan sen tulee olla main!
+    Mikäli haluat lisätä omia tyylejäsi tähän osioon sinun tulee antaa osiolle "ylimääräinen" class määrittely, jota käsittelet
+    omissa tyylimäärittelyissäsi</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus et lectus vulputate, mattis arcu ac, venenatis nibh. Nunc ac augue quam. Suspendisse potenti. Donec blandit urna mauris, vel cursus elit pretium at. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam consequat non erat vitae commodo. Cras molestie facilisis tortor, at dignissim urna sodales nec. Nunc eu ligula volutpat, scelerisque magna nec, ullamcorper nunc. Nullam non quam imperdiet, condimentum felis eget, eleifend velit. Praesent lobortis pulvinar velit, quis pulvinar ipsum condimentum nec. Nam sem massa, pellentesque vel ultricies eget, volutpat eget nulla. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nullam at malesuada sem, porttitor posuere urna. Aenean a scelerisque mi.</p>
+  </div>
+</section>
+  <!-- Secondary osio (lisää eri niin tärkeä sisältö tänne) -->
+<section class="secondary">
+  <div class="wrap">
+    <h2>Väliotsikko</h2>
+    <hr>
+    <p>Tähän tulee kaikki ei niin tärkeä sisältö, joissain tilanteissa tämä voi olla myös piillotettu ja tulee näkyviin vain käyttäjän
+    halutessa nähdä epäolennaista sisältöä. Mikäli haluat lisätä omia tyylejäsi tähän osioon sinun tulee antaa osiolle "ylimääräinen" 
+    class määrittely, jota käsittelet omissa tyylimäärittelyissäsi</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus et lectus vulputate, mattis arcu ac, venenatis nibh. Nunc ac augue quam. Suspendisse potenti. Donec blandit urna mauris, vel cursus elit pretium at. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam consequat non erat vitae commodo. Cras molestie facilisis tortor, at dignissim urna sodales nec. Nunc eu ligula volutpat, scelerisque magna nec, ullamcorper nunc. Nullam non quam imperdiet, condimentum felis eget, eleifend velit. Praesent lobortis pulvinar velit, quis pulvinar ipsum condimentum nec. Nam sem massa, pellentesque vel ultricies eget, volutpat eget nulla. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nullam at malesuada sem, porttitor posuere urna. Aenean a scelerisque mi.</p>
+  </div>
+</section>
+  <!-- Footer osio (ÄLÄ KOSKE!) -->
+<section class="footer">
+	<div class="row">
+    <div class="large-4 column">
+		<h4><i class="fi-torsos-all"></i> SoMETTista</h4>
+		<hr>
+		<p>SoMETTi on Erikoistietotoimiston Musketin sosiaalisenmedian nettiversio,
+		joka mahdollistaa tavallisten ihmisten kuvien lähettämisen yhteisön ja
+		Erikoistietotoimiston työntekijöiden tutkittavaksi.</p>
+	</div>
+    <div class="large-4 column">
+		<h4><i class="fi-book"></i> Käyttäjäehdot</h4>
+		<hr>
+		<p>Huomioithan, että sinulla tulee olla oikeudet lähettämääsi kuvaan ja
+		kuva ei saa sisältää mitään sopimatonta materiaalia tai se tullaan 
+		poistamaan palvelusta lopullisesti.</p>
+	</div>
+    <div class="large-4 column">
+		<h4><i class="fi-magnifying-glass"></i> Löydä meidät</h4>
+		<hr>
+		  <a href="https://fi-fi.facebook.com/"><i class="fi-social-facebook"></i></a>&nbsp;
+		  <a href="https://twitter.com/?lang=fi"><i class="fi-social-twitter"></i></a>&nbsp; 
+		  <a href="https://fi.pinterest.com/"><i class="fi-social-pinterest"></i></a>&nbsp;
+		  <a href="https://www.reddit.com/"><i class="fi-social-reddit"></i></a>&nbsp;
+		  <a href="https://plus.google.com/"><i class="fi-social-google-plus"></i></a>
+	</div>
+	</div>
+</section>
+<script src="http://cosmo.kpedu.fi/~miiahuvila/somett/SoMETT-1.0/dad/node_modules/foundation-sites/vendor/jquery/dist/jquery.min.js"></script>
+<script src="https://cdn.jsdelivr.net/foundation/6.2.3/foundation.min.js"></script>
+<script src="http://cosmo.kpedu.fi/~tomijylha/SoMETT-1.0/dad/dropzone-4.3.0/dist/dropzone.js"></script>
+<script>
+      $(document).foundation();
+</script>
+<script>
+      $(document).ready(function(){
+        $(".apua").click(function(){
+          $(".help").toggle(1000);
+        });
+      });
+</script>
+</div>
+</div>
+</div>
+</body>
+</html>
+
