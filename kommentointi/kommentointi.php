1b71d93d0cf77aaa00e8cb620579c20d18f2feb8
jkeh6 <johannes.kallinen@student.kpedu.fi>
2016-09-22 08:26:19 +0300

    Vetää myös facebook kommentteja mutta vituillaan

--- kommentointi/kommentointi.php
+++ kommentointi/kommentointi.php
@@ -3,6 +3,7 @@ session_start();
 $kuva = $_SESSION['kuvaid'];
 $logged_user = $_SESSION['login_user'];
 $logged_fbuser = $_SESSION['FULLNAME'];
+$faceid = $_SESSION['FBID'];
 $my = mysqli_connect("localhost", "data15", "aJrHfybLxsLU76rV", "data15");
 if ($my->mysqli_errno) {
   die("MySQL, virhe yhteyden luonnissa:" . $my->connect_error);
@@ -42,7 +43,11 @@ $submit = $_POST['submit'];
 if (isset($_POST['submit'])) {
   $obj = $result3->fetch_object();
     var_dump($obj);
-  $sql = "INSERT INTO 581D_Kommentti (UID,Kommentti) VALUES ('".$obj->UID."','$comment') ";
+if ($_SESSION['FBID']) {
+	$sql = "INSERT INTO 581D_Kommentti (UID,Kommentti) VALUES ('".$faceid."','$comment') ";
+} else {
+    $sql = "INSERT INTO 581D_Kommentti (UID,Kommentti) VALUES ('".$obj->UID."','$comment') ";
+}
   $result = $my->query($sql);
 #die($sql);
 
@@ -67,7 +72,7 @@ echo '<h5 class="float-left">&nbsp•&nbsp' . $numrows . '</h5>';
         </div>
         <div>
 <?php
-$result = $my->query("SELECT * FROM 581D_Kommentti, 581D_Kayttaja WHERE 581D_Kommentti.UID = 581D_Kayttaja.UID ORDER BY KTime DESC");
+$result = $my->query("SELECT * FROM 581D_Kommentti, 581D_Kayttaja, 3972_FBKayttaja WHERE 581D_Kommentti.UID = 581D_Kayttaja.UID or 581D_Kommentti.UID = 3972_FBKayttaja.Fuid ORDER BY KTime DESC");
 while ($rows = $result->fetch_array(MYSQLI_ASSOC)) {
   $id = $rows['UID'];
   $uid = $rows['Nimi'];
