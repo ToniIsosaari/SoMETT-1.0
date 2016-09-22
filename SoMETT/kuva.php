77b5fb5b20fb58ef2c4d45264f2161665b0c5c4e
MiiaHuvila <mangamirkku@gmail.com>
2016-09-22 09:06:20 +0300

    fixi

--- SoMETT/kuva.php
+++ SoMETT/kuva.php
@@ -59,49 +59,15 @@ if ($my->mysqli_errno) {
 <meta property="article:section" content="Article Section" />
 <meta property="article:tag" content="Article Tag" />
 <meta property="fb:admins" content="Facebook numberic ID" />
-
-<link rel="stylesheet" href="http://cosmo.kpedu.fi/~tomijylha/SoMETT-1.0/dad/dropzone-4.3.0/dist/dropzone.css">
-<link rel="stylesheet" href="http://cosmo.kpedu.fi/~miiahuvila/somett/SoMETT-1.0/dad/node_modules/foundation-sites/dist/foundation.min.css">
+<?php include("styles.php");?>
 <link rel="stylesheet" href="foundation-icons/foundation-icons.css">
 <link rel="stylesheet" href="css/foundation-icons.min.css" rel="stylesheet" type="text/css" />
 <link rel="stylesheet" href="css/stylesheet.css" />
-<link rel="stylesheet" href="http://cosmo.kpedu.fi/~miiahuvila/somett/SoMETT-1.0/SoMETT/tyylit.css">
 </head>
 
 
 <body>
-<div class="off-canvas-wrapper">
- <div class="off-canvas-wrapper-inner" data-off-canvas-wrapper>
-    <div class="off-canvas position-left" id="offCanvas" data-off-canvas>
-  <!-- Mobiili Navipalkki (ÄLÄ KOSKE!) -->
-<ul class="vertical menu mobile-menu">
- <li><a href="http://cosmo.kpedu.fi/~miiahuvila/somett/SoMETT-1.0/SoMETT/index.php">Etusivu</a></li>
- <li><a href="#">Kuvapankki</a></li>
- <li><a href="#">Lähetyspalvelu</a></li>
-</ul>
-
-</div>
-
-<div class="off-canvas-content" data-off-canvas-content>
-   <div class="title-bar nav-mobile">
-  <div class="title-bar-left">
-    <button class="menu-icon" type="button" data-open="offCanvas"></button>
-    <span class="title-bar-title">SoMETT</span>
-  </div>
-</div>
-  <!-- Poytakone Navipalkki (ÄLÄ KOSKE!) -->
-<div class="top-bar nav-desktop">
-<div class="wrap">
-  <div class="top-bar-left">
-    <ul class="dropdown menu dropdown-desktop" data-dropdown-menu>
-      <li class="menu-text">SoMETT</li>
-      <li><a href="http://cosmo.kpedu.fi/~miiahuvila/somett/SoMETT-1.0/SoMETT/index.php">Etusivu</a></li>
-      <li><a href="#">Kuvapankki</a></li>
-      <li><a href="#">Lähetyspalvelu</a></li>
-    </ul>
-  </div>
-</div>
-</div>
+<?php include("nav.php");?>
   <!-- Main osio (lisää sivulle olennainen sisältö tänne) -->
 <section class="main">
   <div class="wrap">
@@ -200,57 +166,8 @@ $my->close();
   </div>
 </section>
   <!-- Footer osio (ÄLÄ KOSKE!) -->
-<section class="footer">
-    <div class="row">
-    <div class="large-4 column">
-        <h4><i class="fi-torsos-all"></i> SoMETTista</h4>
-        <hr>
-        <p>SoMETTi on Erikoistietotoimiston Musketin sosiaalisenmedian nettiversio,
-        joka mahdollistaa tavallisten ihmisten kuvien lähettämisen yhteisön ja
-        Erikoistietotoimiston työntekijöiden tutkittavaksi.</p>
-    </div>
-    <div class="large-4 column">
-        <h4><i class="fi-book"></i> Käyttäjäehdot</h4>
-        <hr>
-        <p>Huomioithan, että sinulla tulee olla oikeudet lähettämääsi kuvaan ja
-        kuva ei saa sisältää mitään sopimatonta materiaalia tai se tullaan
-        poistamaan palvelusta lopullisesti.</p>
-    </div>
-    <div class="large-4 column">
-        <h4><i class="fi-magnifying-glass"></i> Löydä meidät</h4>
-        <hr>
-          <a href="https://fi-fi.facebook.com/"><i class="fi-social-facebook"></i></a>&nbsp;
-          <a href="https://twitter.com/?lang=fi"><i class="fi-social-twitter"></i></a>&nbsp;
-          <a href="https://fi.pinterest.com/"><i class="fi-social-pinterest"></i></a>&nbsp;
-          <a href="https://www.reddit.com/"><i class="fi-social-reddit"></i></a>&nbsp;
-          <a href="https://plus.google.com/"><i class="fi-social-google-plus"></i></a>
-    </div>
-    </div>
-</section>
-<script src="http://cosmo.kpedu.fi/~miiahuvila/somett/SoMETT-1.0/dad/node_modules/foundation-sites/vendor/jquery/dist/jquery.min.js"></script>
-<script src="https://cdn.jsdelivr.net/foundation/6.2.3/foundation.min.js"></script>
-<script src="http://cosmo.kpedu.fi/~tomijylha/SoMETT-1.0/dad/dropzone-4.3.0/dist/dropzone.js"></script>
-<script>
-      $(document).foundation();
-</script>
-<script>
-      $(document).ready(function(){
-        $(".apua").click(function(){
-          $(".help").toggle(1000);
-        });
-      });
-</script>
-<script>
-  function openWin() {
-  window.location.href = "kommentointi_login.php";
-  }
-</script>
-<script>
-$('p#kommentti').readmore({
-  moreLink: '<a href="#" class="more">Näytä enemmän <i class="fi-arrow-down"></i></a>',
-  lessLink: '<a href="#" class="less">Näytä vähemmän <i class="fi-arrow-up"></i></a>',
-});
-</script>
+<?php include("footer.php")?>
+<?php include("script.php")?>
 </div>
 </div>
 </div>
