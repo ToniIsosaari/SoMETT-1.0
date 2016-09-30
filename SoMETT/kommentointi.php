6a6211cf053eb4a4cfbbd2706b5c5b47ca9a7cfa
jkeh6 <johannes.kallinen@student.kpedu.fi>
2016-09-30 18:09:17 +0300

    korjattu uudelleenohjaukset/lisätty napit/yms

--- SoMETT/kommentointi.php
+++ SoMETT/kommentointi.php
@@ -1,11 +1,11 @@
 <?php
+session_start();
   $my = mysqli_connect('localhost', 'data15', 'aJrHfybLxsLU76rV', 'data15');
   if ($my->mysqli_errno) {
       die('MySQL, virhe yhteyden luonnissa:'.$my->connect_error);
   }
   $my->set_charset('utf8');
 
-  session_start();
   $kuvaid = $_GET['KID'];
   $logged_user = $_SESSION['login_user'];
   $logged_fbuser = $_SESSION['FULLNAME'];
@@ -58,25 +58,31 @@
           if (isset($_POST['submit'])) {
             $obj = $result3->fetch_object();
             if ($_SESSION['FBID']) {
-              $sql = "INSERT INTO 581D_Kommentti (UID,Kommentti,KuvaID) VALUES ('".$faceid."','$comment','".$kuvaid."') ";
+              $sql = "INSERT INTO 581D_Kommentti (UID,Kommentti,KuvaID) VALUES ('$faceid','$comment','$kuvaid') ";
             } else {
-              $sql = "INSERT INTO 581D_Kommentti (UID,Kommentti,KuvaID) VALUES ('".$obj->UID."','$comment','".$kuvaid."') ";
+              $sql = "INSERT INTO 581D_Kommentti (UID,Kommentti,KuvaID) VALUES ('".$obj->UID."','$comment','$kuvaid') ";
             }
             $result = $my->query($sql);
             //die($sql);
-            echo "<meta HTTP-EQUIV='REFRESH' content='0; url=kommentointi.php?KID=' . $kuvaid . ' '>";
+            //echo "<meta HTTP-EQUIV='REFRESH' content='0; url=kommentointi.php?KID=".$kuvaid."'>";
+			header("Location: kommentointi.php?KID=".$kuvaid);
           }
         ?>
         <div>
           <form action="kommentointi.php" method="POST">
             <div>
               <h5 class="float-left">Kommentteja</h5>
+			  
               <!--HAETAAN KUVAKOHTAISET KOMMENTIT TIETOKANNASTA-->
               <?php
                 $result1 = $my->query("SELECT * FROM 581D_Kommentti WHERE KuvaID = '$kuvaid'");
                 $numrows = $result1->num_rows;
                 echo '<h5 class="float-left">&nbsp•&nbsp'.$numrows.'</h5>';
               ?>
+              <textarea name="comment" maxlength="140" rows="2" required></textarea>
+              <input class="button float-right" type="submit" name="submit" value="Kommentoi">
+              <a href="<?php echo 'fblogin/logout.php?KID='.$kuvaid.'';?>">Kirjaudu Ulos</a><br>
+              <hr>
               <!--HAETAAN KAIKKI KUVAAN LIITTYVÄT TIEDOT-->
               <?php
                 $result = $my->query("SELECT * FROM 581D_Kommentti, 581D_Kayttaja WHERE 581D_Kommentti.UID = 581D_Kayttaja.UID AND KuvaID = '$kuvaid' ORDER BY KTime DESC");
@@ -140,7 +146,7 @@
               $numrows = $result1->num_rows;
               echo '<h5 class="float-left">&nbsp•&nbsp'.$numrows.'</h5>';
             ?>
-            <input type="button" value="Kirjaudu Sisään" onclick="openWin()">
+			<a href="<?php echo 'kommentointi_login.php?KID='.$kuvaid.'';?>">Kirjaudu Sisään</a><br>
             <hr>
           </div>
         </div>
@@ -191,11 +197,6 @@
        <?
            include 'footer.php';
                ?>
-      <script>
-        function openWin() {
-          window.location.href = "kommentointi_login.php";
-        }
-      </script>
       <script src="js/jquery.min.js"></script>
       <script src="js/what-input.min.js"></script>
       <script src="js/foundation.min.js"></script>
@@ -211,6 +212,3 @@
       </script>
   </body>
 </html>
-
-
-
