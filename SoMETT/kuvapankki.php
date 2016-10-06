aa3567f85a0eb689912e31eb269f0cb480cb6f6a
Juuso Niemi-Korpi <juusoniemi-korpi@cosmo.kpedu.fi>
2016-10-06 14:32:34 +0300

    Galleria lisäys kuvapankkiin

--- SoMETT/kuvapankki.php
+++ SoMETT/kuvapankki.php
@@ -5,6 +5,8 @@
 <meta http-equiv="x-ua-compatible" content="ie=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <title>SoMETT</title>
+<link href="Galleroni.css" rel="stylesheet" type="text/css">
+    <script src="holder.js"></script>
 <?php include("styles.php"); ?>
 </head>
 <body>
@@ -13,15 +15,72 @@
 <section class="main">
   <div class="wrap">
     <h1>Kuva galleria</h1>
-    <p style="font-size: 30px;">[Tulee käyttöön pian]</p>
-    <p>Tässä on sivu, jonka sisältöä ei ole palautettu kokoajille aikarajaan mennessä! Toivottavasti se tulee joskus.</p>
-    <p>[<a href="http://cosmo.kpedu.fi/~miiahuvila/somett/SoMETT-1.0/SoMETT/kuva.php">Linkki sivuun, jonka tulisi olla yhteydessä tähän sivuun</a>]</p>
+    <p style="font-size: 30px;"></p>
+    <p>
+    
+    <p>Tervetuloa kuvagalleriaan. Täältä löydät kaikki kuvat joita palveluumme on ladattu</p>
+
+<form name="form" action="kuvapankki.php" method="post">
+  <select name="jarjestys">
+  <option value="Suosituin">Suosituimmat</option>
+  <option value="Tykkaus">Tykätyimmät</option>
+  <option value="Uusimmat">Uusimmat</option>
+  </select>
+  <input type="submit" value="Järjestä">
+</form>
+<?php
+  $DK = $_POST['jarjestys'];
+?>
+
+
+    <div class="Galleria-div">
+    <?php
+      $my=mysqli_connect("localhost","data15","aJrHfybLxsLU76rV","data15" );
+
+      if($my->mysql_errno){
+        die("Virhe yhteyden luonnissa". $my->connect_error);
+      }
+      $my->set_charset('utf8');
+
+      if($DK == "Suosituin"){
+
+        $sql = "SELECT * FROM 581D_Kuva ORDER BY Suosituin DESC;";
+        $DK = "Suosituimmat";
+      }elseif($DK == "Tykkaus"){
+
+        $sql = "SELECT * FROM 581D_Kuva ORDER BY Tykkaus DESC;";
+        $DK = "Tykätyimmät";
+      }elseif($DK == "Uusimmat"){
+
+        $sql = "SELECT * FROM 581D_Kuva ORDER BY KuvaID DESC;";
+        $DK = "Uusimmat";
+      }else{
+
+        $sql = "SELECT * FROM 581D_Kuva;";
+        $DK = "";
+      }
+
+      echo "<p>$DK</p>";
+      if($result = $my->query($sql)){
+        while($d = $result->fetch_object()){
+          echo "<a class='kuvalinkki' href='http://cosmo.kpedu.fi/~toniisosaari/SoMETT/SoMETT-1.0/SoMETT/kommentointi.php?KID=".$d->KuvaID."'>
+          <object class='img-galleria' data='$d->URL'>
+            <img data-src='holder.js/160x160?text=Kuvaa ei ole olemassa'>
+          </object>
+          <p class='Galleria-otsikko'>$d->Title</p>
+          </a>";
+        }
+      }
+      $my->close();
+
+    ?>
+    </p>
   </div>
 </section>
   <!-- Secondary osio (lisää eri niin tärkeä sisältö tänne) -->
 <section class="secondary grey">
   <div class="wrap">
-    <h2>Rip Juuso 2K16</h2>
+    <h2></h2>
     <hr>
     <p>Tähän tulee kaikki ei niin tärkeä sisältö, joissain tilanteissa tämä voi olla myös piillotettu ja tulee näkyviin vain käyttäjän
     halutessa nähdä epäolennaista sisältöä. Mikäli haluat lisätä omia tyylejäsi tähän osioon sinun tulee antaa osiolle "ylimääräinen" 
