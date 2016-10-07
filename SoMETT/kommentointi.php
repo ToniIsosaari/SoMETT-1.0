381b66c4d3a4be0ffb33f738a3ce99fdf470ac1d
Niko Lipponen <nikolipponen@cosmo.kpedu.fi>
2016-10-07 09:44:31 +0300

    lisätty upload päivämäärä

--- SoMETT/kommentointi.php
+++ SoMETT/kommentointi.php
@@ -168,13 +168,14 @@ session_start();
       <!--JOS KÄYTTÄJÄ ON KIRJAUTUNUT LOPPUU JA JOS KÄYTTÄJÄ EI OLE KIRJAUTUNUT ALKAA-->
     <?php else: ?>
     <?php
-      $sql = "SELECT URL FROM 581D_Kuva WHERE KuvaID = '$kuvaid'";
+      $sql = "SELECT URL, PublishDate FROM 581D_Kuva WHERE KuvaID = '$kuvaid'";
       //echo $sql;
       $result = $my->query($sql);
       $kkysely = $result->fetch_object();
     ?>
 
     <img src=<?php echo  $kkysely->URL; ?> width="300" height="400" name="image" />
+    <?echo "Kuva on ladattu: ".$kkysely->PublishDate;?>    
     <div class="row">
       <div class="panel">
         <div>
