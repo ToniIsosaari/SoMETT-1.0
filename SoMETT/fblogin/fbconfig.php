46194b9a1dc48451065bebb25e231eb92e65d8e9
jkeh6 <johannes.kallinen@student.kpedu.fi>
2016-10-10 16:28:42 +0300

    kirjautuminen facebookilla toimii nyt etusivulta ja kommenteista

--- SoMETT/fblogin/fbconfig.php
+++ SoMETT/fblogin/fbconfig.php
@@ -1,10 +1,10 @@
 <?php
 session_start();
-$kuvaid = $_GET['KID'];
+$kuvaid    = $_GET['KID'];
 $hakemisto = $_SERVER['SERVER_NAME'] . dirname($_SERVER['REQUEST_URI']);
 // added in v4.0.0
 require_once 'autoload.php';
-require 'functions.php'; 
+require 'functions.php';
 use Facebook\FacebookSession;
 use Facebook\FacebookRedirectLoginHelper;
 use Facebook\FacebookRequest;
@@ -17,38 +17,50 @@ use Facebook\Entities\AccessToken;
 use Facebook\HttpClients\FacebookCurlHttpClient;
 use Facebook\HttpClients\FacebookHttpable;
 // init app with app id and secret
-FacebookSession::setDefaultApplication( '1216316551765571','80b03311c222c56dc432584c448890ec' );
+FacebookSession::setDefaultApplication('1216316551765571', '80b03311c222c56dc432584c448890ec');
 // login helper with redirect_uri
-    $helper = new FacebookRedirectLoginHelper('http://'.$hakemisto.'/fbconfig.php?KID='.$kuvaid.'' );
+
+if (isset($_GET['KID'])) {
+    $helper = new FacebookRedirectLoginHelper('http://' . $hakemisto . '/fbconfig.php?KID=' . $kuvaid . '');
+} else {
+    $helper = new FacebookRedirectLoginHelper('http://' . $hakemisto . '/fbconfig.php');
+}
 try {
-  $session = $helper->getSessionFromRedirect();
-} catch( FacebookRequestException $ex ) {
-  // When Facebook returns an error
-} catch( Exception $ex ) {
-  // When validation fails or other local issues
+    $session = $helper->getSessionFromRedirect();
+}
+catch (FacebookRequestException $ex) {
+    // When Facebook returns an error
+}
+catch (Exception $ex) {
+// When validation fails or other local issues
 }
 // see if we have a session
-if ( isset( $session ) ) {
-  // graph api request for user data
-  $request = new FacebookRequest( $session, 'GET', '/me?locale=en_US&fields=id,name,email' );
-  $response = $request->execute();
-  // get response
-  $graphObject = $response->getGraphObject();
-     	$fbid = $graphObject->getProperty('id');              // To Get Facebook ID
- 	    $fbfullname = $graphObject->getProperty('name'); // To Get Facebook full name
-	    $femail = $graphObject->getProperty('email');    // To Get Facebook email ID
-	/* ---- Session Variables -----*/
-	    $_SESSION['FBID'] = $fbid;           
-        $_SESSION['FULLNAME'] = $fbfullname;
-	    $_SESSION['EMAIL'] =  $femail;
+if (isset($session)) {
+    // graph api request for user data
+    $request              = new FacebookRequest($session, 'GET', '/me?locale=en_US&fields=id,name,email');
+    $response             = $request->execute();
+    // get response
+    $graphObject          = $response->getGraphObject();
+    $fbid                 = $graphObject->getProperty('id'); // To Get Facebook ID
+    $fbfullname           = $graphObject->getProperty('name'); // To Get Facebook full name
+    $femail               = $graphObject->getProperty('email'); // To Get Facebook email ID
+    /* ---- Session Variables -----*/
+    $_SESSION['FBID']     = $fbid;
+    $_SESSION['FULLNAME'] = $fbfullname;
+    $_SESSION['EMAIL']    = $femail;
     /* ---- header location after session ----*/
-//  checkuser($fuid,$ffname,$femail);
-    checkuser($fbid,$fbfullname,$femail);
-  header("Location: ../../SoMETT/kommentointi.php?KID=".$kuvaid);
+    //  checkuser($fuid,$ffname,$femail);
+    checkuser($fbid, $fbfullname, $femail);
+    
+    if (isset($_GET['KID'])) {
+        header("Location: ../../SoMETT/kommentointi.php?KID=" . $kuvaid);
+    } else {
+        header("Location: ../../SoMETT/index.php");
+    }
 } else {
-$loginUrl = $helper->getLoginUrl(array(
-'scope' => 'email'
-));
-header("Location: ".$loginUrl);
+    $loginUrl = $helper->getLoginUrl(array(
+        'scope' => 'email'
+    ));
+    header("Location: " . $loginUrl);
 }
 ?>
