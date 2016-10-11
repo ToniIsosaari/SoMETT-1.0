8cc4aa40dd4885c2f1eb6af936aac16482723f8f
jkeh6 <johannes.kallinen@student.kpedu.fi>
2016-10-11 17:22:37 +0300

    update

--- SoMETT/fblogin/logout.php
+++ SoMETT/fblogin/logout.php
@@ -5,5 +5,10 @@ $kuvaid = $_GET['KID'];
     $_SESSION['FBID'] = NULL;
     $_SESSION['FULLNAME'] = NULL;
     $_SESSION['EMAIL'] =  NULL;
-header("Location: ../../SoMETT/index.php");        // you can enter home page here ( Eg : header("Location: " ."http://www.krizna.com"); 
+			  						    if (isset($_GET['KID'])) {
+        echo '<META HTTP-EQUIV="Refresh" Content="0; URL=../../SoMETT/kommentointi.php?KID=' . $kuvaid . '">'; 
+    } else {
+        echo '<META HTTP-EQUIV="Refresh" Content="0; URL=../../SoMETT/index.php">'; 
+    }
+//header("Location: ../../SoMETT/index.php");        // you can enter home page here ( Eg : header("Location: " ."http://www.krizna.com"); 
 ?>
